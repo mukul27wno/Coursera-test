bcc84629182b999487c09fc256ccd99adacad7ad
Mukul <mukul27wno@gmail.com>
2021-11-12 12:39:41 +0530

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+(function ()
+{
+    'use strict';
+
+    angular.module('NameCalculator',[])
+    .controller('NameCalculatorController',function($scope){
+    $scope.name ="";
+    $scope.totalvalue = 0;
+    
+    }
+    
+    
+
+})
